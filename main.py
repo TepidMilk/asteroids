be4a70cdbf874a3a0e96c3e506fffcc74fa54a7f
TepidMilk <ivorykingrey@gmail.com>
2024-09-18 22:04:57 -0500

    Add player class Update Constants to include player size Update main to draw player to screen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
+from circleshape import *
+from player import *
 from constants import *
+
 
 pygame.init
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
@@ -7,16 +10,19 @@ clock = pygame.time.Clock()
 dt = 0
 
 def main():
+    player = Player(SCREEN_WIDTH//2, SCREEN_HEIGHT//2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     while True:
+        screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
+        player.draw(screen)
         tick = clock.tick(60)
         dt = tick / 1000
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
