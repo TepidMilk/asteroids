fd149dd78fdebaa9352d8a11f46ebf22f8fe8bcc
TepidMilk <ivorykingrey@gmail.com>
2024-09-19 13:32:51 -0500

    Add movement to player class Add player speed to constants Add groups and player movement to main

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ pygame.init
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
 clock = pygame.time.Clock()
 dt = 0
+updateable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updateable, drawable)
 
 def main():
     player = Player(SCREEN_WIDTH//2, SCREEN_HEIGHT//2)
@@ -15,12 +18,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     while True:
         screen.fill("black")
+        tick = clock.tick(60)
+        dt = tick / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.draw(screen)
-        tick = clock.tick(60)
-        dt = tick / 1000
+        for items in updateable:
+            items.update(dt)
+        for items in drawable:
+            items.draw(screen)
         pygame.display.flip()
 
 
