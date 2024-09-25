f3598cd5a6676292e97dfbb7412cce41470bc288
TepidMilk <ivorykingrey@gmail.com>
2024-09-25 14:01:10 -0500

    add comments to code for better organization and readability

--- player.py
+++ player.py
@@ -18,16 +18,27 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
+    #draws player to screen
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
+
+    #allow player to rotate on screen
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
 
+    #allows player to move backwards and forwards
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
+    #allows player to spawn a shot object
+    def shoot(self, dt):
+        self.shot = Shot(self.position.x, self.position.y)
+        self.shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+        
+    """allows player to update movement through key presses
+    which call rotate, move, and shoot methods"""
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -45,19 +56,17 @@ class Player(CircleShape):
                 Player.timer = PLAYER_SHOT_COOLDOWN
         Player.timer -= dt
     
-    def shoot(self, dt):
-        self.shot = Shot(self.position.x, self.position.y)
-        self.shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOT_SPEED
-        
 
 
-    
+#shot class to destroy asteroids
 class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
     
+    #draws shot to screen
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, SHOT_RADIUS, 2)
 
+    #updates movement of shot 
     def update(self, dt):
         self.position += (self.velocity * dt)
